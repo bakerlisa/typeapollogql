1cb5d72fbaf8adf8f96c045976056e507a4b757a
Lisa <lb.broadhead@gmail.com>
2022-10-06 09:14:06 -0600

    first commit

--- client/src/components/Banner/index.tsx
+++ client/src/components/Banner/index.tsx
@@ -1,5 +1,6 @@
 import { printIntrospectionSchema } from 'graphql';
 import React, { FC } from 'react';
+import style from './style';
 
 interface bannerInfo {
     title: string,
@@ -9,7 +10,7 @@ interface bannerInfo {
 }
 
 const Banner: FC<bannerInfo> = (props): JSX.Element => (
-    <div>
+    <div classes={style.banner}>
         <h1>{props.name}, <br/> {props.title}</h1>
         <h1><strong>Mission: </strong>{props.mission}</h1>
         <p>you have {props.respond} to respond</p>
---
+++ client/src/components/Banner/style.ts
@@ -0,0 +1,6 @@
+export default {
+    banner: {
+        backgroundColor: 'black',
+        color: 'white',
+    },
+};
